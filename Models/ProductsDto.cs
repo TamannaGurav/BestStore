86ed6a5b7a0fdac7afe8e6f368789fb845d8fce5
TamannaGurav <tamannagurav13@gmail.com>
2024-08-28 04:14:38 +0530

    Add project files.

---
+++ Models/ProductsDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestStore.Models
+{
+    public class ProductsDto
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Required]
+        public string Description { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+
+        public string? ImageFileName { get; set; }
+    }
+}
---
+++ Services/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BestStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStore.Services
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
